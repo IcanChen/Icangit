5c05dbf5ccafe440497e1aa9c9e59500fcc4567e
Ican Chen <chenmingjianwork@163.com>
2016-01-04 22:37:04 +0800

    modified LinearList  add iterator

--- DataStructures/LinearList/include/arrayList.h
+++ DataStructures/LinearList/include/arrayList.h
@@ -43,6 +43,54 @@ class arrayList : public linearList<T>
 		//additional method
 		int capacity() const {return arrayLength;}
 
+		//iterator to start and end of list
+		class iterator;
+		iterator begin(){return iterator(element);}
+		iterator end(){return iterator(element + listSize);}
+
+		//iterator for arrayList.
+		class iterator
+		{
+			public:
+				// typedefs required by C++ for a bidirectional iterator
+				typedef bidirectional_iterator_tag iterator_category;
+				typedef T value_type;
+				typedef ptrdiff_t difference_type;
+				typedef T* pointer;
+				typedef T& reference;
+
+				//constructor
+				iterator(T* thePosition = 0){position = thePosition;}
+
+				//dereferencing operators
+				T& operator*() const{return *position;}
+				T* operator->() const{return &*position;}
+
+				//increment
+				iterator& operator++()//preincrement
+					{++position;return *this;}
+				iterator operator++(int) //postincrement
+					{iterator old = *this;
+					 ++position;
+					 return old;
+					}
+				//decrement
+				iterator& operator--() //predecrement
+					{--position;return *this;}
+				iterator operator--(int) //postdecrement
+					{iterator old = *this;
+					 --position;
+					 return old;				 
+					}
+
+				//equality testing
+				bool operator!=(const iterator right) const
+					{return position != right.position;}
+				bool operator==(const iterator right) const
+					{return position == right.position;}
+			protected:
+				T* position;
+		};
 	protected:
 		void checkIndex(int theIndex) const;
 
